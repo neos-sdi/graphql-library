11add56d69a2d75470620d38cd8efa40367ae715
Lionel Dimitrijevic <lionel.dimitrijevic@neos-sdi.com>
2022-01-18 16:03:11 +0100

    Entity creations

---
+++ Library.Domain/Entities/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Domain.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public Guid? AuthorId { get; set; }
+        public Guid? LanguageId { get; set; }
+        public Guid? PublisherId { get; set; }
+        public Guid? SerieId { get; set; }
+        public Author? Author { get; set; }
+        public Language? Language { get; set; }
+        public Publisher? Publisher { get; set; }
+        public Serie? Serie { get; set; }
+    }
+}
