5d2ed4edc43fa85f73a5ae17ba5c2c394304eac8
Lionel Dimitrijevic <lionel.dimitrijevic@neos-sdi.com>
2022-01-19 08:41:34 +0100

    Query et mutation de base

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,6 +1,37 @@
+using GraphQL.Mutations;
+using GraphQL.Queries;
+using Library.Application.Books.Commands;
+using Library.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigureServices(builder.Services);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGraphQL();
+
+    endpoints.MapGet("/", context =>
+    {
+        context.Response.Redirect("/graphql", true);
+        return Task.CompletedTask;
+    });
+});
 
 app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+    string connectionString = "server=localhost;user=sa;pwd=P@ssword;database=Library";
+    services.AddDbContext<LibraryDbContext>(options => options.UseSqlServer(connectionString));
+
+    services.AddGraphQLServer()
+            .AddQueryType()
+            .AddMutationType()
+            .AddTypeExtension<BookQueries>()
+            .AddTypeExtension<BookMutations>();
+
+}
---
+++ Library.Application/Books/Commands/AddBookCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Application.Books.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public record AddBookCommand(string Title, string? Description);
+    
+}
